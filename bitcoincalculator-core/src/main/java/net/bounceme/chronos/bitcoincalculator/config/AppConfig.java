7c74da950cf51f5d2457abb32ac759ea39c4d91a
federicomartinlara <federicomartinlara1976@gmail.com>
2018-03-10 23:31:04 +0100

    Changes for jboss EAP

--- bitcoincalculator-core/src/main/java/net/bounceme/chronos/bitcoincalculator/config/AppConfig.java
+++ bitcoincalculator-core/src/main/java/net/bounceme/chronos/bitcoincalculator/config/AppConfig.java
@@ -1,5 +1,10 @@
 package net.bounceme.chronos.bitcoincalculator.config;
 
+import javax.annotation.Resource;
+
+import org.infinispan.manager.DefaultCacheManager;
+import org.infinispan.spring.provider.SpringEmbeddedCacheManager;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -10,12 +15,12 @@ import net.bounceme.chronos.bitcoincalculator.dto.BitcoinCalculatorDTO;
 import net.bounceme.chronos.bitcoincalculator.dto.ExchangeDTO;
 import net.bounceme.chronos.bitcoincalculator.messages.MessageProperties;
 import net.bounceme.chronos.bitcoincalculator.messages.PubliProperties;
-import net.bounceme.chronos.utils.cache.config.RemoteCacheConfiguration;
 import net.bounceme.chronos.utils.mapping.JacksonConverter;
 import net.bounceme.chronos.utils.net.json.JSONPool;
 
 @Configuration
-@Import({RemoteCacheConfiguration.class, LogPerformanceConfig.class})
+@EnableCaching
+@Import({LogPerformanceConfig.class})
 @PropertySource(value={"classpath:application.properties"})
 public class AppConfig {
 	public static final String BITCOIN_CONVERTER = "bitcoinConverter";
@@ -23,6 +28,9 @@ public class AppConfig {
 	public static final String EXCHANGE_CONVERTER = "exchangeConverter";
 	
 	public static final String CACHE = "bitcoin-calculator";
+	
+	@Resource(lookup="java:jboss/infinispan/container/BitcoinCalculator") 
+	DefaultCacheManager cacheManager;
 	
 	@SuppressWarnings({ "unchecked", "rawtypes" })
 	@Bean(name = BITCOIN_CONVERTER)
@@ -55,5 +63,10 @@ public class AppConfig {
     @Bean
 	public JSONPool jsonPool() {
 		return new JSONPool();
+	}
+    
+    @Bean
+	public SpringEmbeddedCacheManager cacheManager() {
+		return new SpringEmbeddedCacheManager(cacheManager);
 	}
 }
